0fe82e7d697b8a082bec2f5d4505d21c2fda6cca
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-08-06 14:55:33 +0100

    Adding the header to portfolio

---
+++ src/Frontend/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+   return (
+      <div className="header">
+         <div class="header__text-box">
+            <h1 className="heading-primary">
+               <span className="heading-primary-name">Chris Woolnough</span>
+               <span className="heading-primary-jobTitle">Frontend Developer</span>
+            </h1>
+         </div>
+      </div>
+   )
+}
+
+export default Header;
